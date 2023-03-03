a20edc52fe5cc0f787139318c238f65c7573f9a1
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-03-03 04:17:36 -0500

    basic architecture and react icons and google fonts and color variables added

---
+++ README.old.md
@@ -0,0 +1,3 @@
+## My portfolio : `vikramsingh.tech`
+
+### Deployed with Vercel
---
+++ src/components/cardList/CardList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Card from '../card/Card';
+
+const CardList = () => {
+    return <div></div>;
+};
+
+export default CardList;
