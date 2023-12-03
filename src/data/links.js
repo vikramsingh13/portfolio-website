b924b77e0bb5c5868a34d6bb2b79a251ce1744c7
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-12-02 22:35:03 -0500

    updated resume link

--- src/data/links.js
+++ src/data/links.js
@@ -2,6 +2,6 @@ export const links = {
         linkedin: "https://www.linkedin.com/in/vikram-singh-32b0b2119/",
         github: "https://github.com/vikramsingh13",
         codepen: "https://codepen.io/vikramsingh13",
-        resume: "https://drive.google.com/file/d/1a1zuijoBESgKugw2RVrVydQ90iIWYO2z/view?usp=share_link",
+        resume: "https://drive.google.com/file/d/1CDURmBFhxGZF4NSt7U_z0Ev_xaqvUWR7/view?usp=drive_link",
         
 };
